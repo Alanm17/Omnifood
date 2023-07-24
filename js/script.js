10fd0c58f8e09f625c925a4ea878715912ce5213
Alanm17 <mukhammadazizkholdarov@gmail.com>
2023-07-24 19:48:06 +0900

    ...

--- js/script.js
+++ js/script.js
@@ -9,11 +9,23 @@ console.log("myName");
 //   h1.style.backgroundColor = "red";
 //   h1.style.padding = "5rem";
 // });
+// ///////////////////////////
+// set current year
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+// ///////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+})("nav-open");
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
